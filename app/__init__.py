d333265bce5ad793cba47839c3dda2642e192bd0
ahow6552 <a0961500945@gmail.com>
2025-05-20 15:14:51 +0800

    Update README and api.yml

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flasgger import Swagger
 from app.api import register_routes
 
 def create_app():
     app = Flask(__name__)
+    swagger = Swagger(app, template_file='../api.yml')
     register_routes(app)
     return app

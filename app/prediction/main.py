6aaf8a33b0099c7c5af73e1aff9f6bd48a95b5d4
ahow6552 <a0961500945@gmail.com>
2025-06-11 22:25:08 +0800

    Add naive predictor test

--- app/prediction/main.py
+++ app/prediction/main.py
@@ -1,29 +1,12 @@
 from data_loader import load_parking_data
-import pandas as pd
-
-def data():
-    data = load_parking_data()
-    return data
-
-def available(df):
-    # 只保留主欄位與展開的 SpaceType = 1 資訊
-    def extract_type_1(avail_list):
-        for item in avail_list:
-            if item["SpaceType"] == 1:
-                return item["AvailableSpaces"], item["NumberOfSpaces"]
-        return None, None
-
-    df[["type1_avail", "type1_total"]] = df["Availabilities"].apply(
-        lambda x: pd.Series(extract_type_1(x))
-    )
-    return df
+from predictor import train_model
 
 def test(data):
-    df = pd.DataFrame(data)
-    print("資料筆數：", len(df))    
+    print("資料筆數：", len(data))    
+    print(data.head())
 
 if __name__ == "__main__":
-    data()
-
-
-    
+    data = load_parking_data()
+    print(type(data))  # 確認是 DataFrame
+    test(data)
+    train_model(data)  # 直接用 load_parking_data 回傳的 DataFrame 訓練

0e8e660b310689cc73aa7dca833e3cf4a643586a
ahow6552 <a0961500945@gmail.com>
2025-06-18 02:02:57 +0800

    !Hide line key

--- app/linebot/webhook.py
+++ app/linebot/webhook.py
@@ -10,10 +10,8 @@ import os
 linebot_bp = Blueprint('linebot', __name__)
 
 # 載入你的 LINE Channel Access Token 與 Secret（建議用 .env 管理）
-# LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
-# LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
-LINE_CHANNEL_ACCESS_TOKEN = REMOVED"="
-LINE_CHANNEL_SECRET = REMOVED""
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
